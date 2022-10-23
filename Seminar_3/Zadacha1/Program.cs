5f9618c08597d350f0b7d5211103a9b26650710b
Igor Gainiiakhmetov <garikahmet@yandex.ru>
2022-10-23 22:29:09 +0300

    seminar3

---
+++ Seminar_3/Zadacha1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// <Возвращаемый тип данных> <наименование метода>(переменные) { тело метода }
+// получить номер четверти на плоскости
+
+int GetPointArea(int coordX, int coordY)
+{
+    int numberArea = 0;
+    if (coordX > 0 && coordY > 0)
+    {
+        numberArea = 1; // вместо этого можно написать return 1
+    }
+
+    if (coordX < 0 && coordY > 0)
+    {
+        numberArea = 2; // вместо этого можно написать return 2
+    }
+
+    if (coordX < 0 && coordY < 0)
+    {
+        numberArea = 3;  // вместо этого можно написать return 3
+    }
+
+    if (coordX > 0 && coordY < 0)
+    {
+        numberArea = 4;   // вместо этого можно написать return 4
+    }
+    return numberArea;    // вместо этого можно написать return 0 соответственно 
+}
+Console.WriteLine("Введи X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int numberArea = GetPointArea(x, y);  // метод
+Console.WriteLine(numberArea);
+
