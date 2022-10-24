38a09d575a7ae88123dc394566d87f5105bf6ebf
Igor Gainiiakhmetov <garikahmet@yandex.ru>
2022-10-24 22:26:35 +0300

    seminar3

---
+++ Seminar_3/Zadacha2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+string GetCoordArea(int chetvert)
+{
+string coords = "";
+if (chetvert == 1)
+{
+    coords = "x >0, y > 0";
+}
+if (chetvert == 2)
+{
+    coords = "x < 0, y > 0";
+}
+if (chetvert == 3)
+{
+    coords = "x < 0, y < 0";
+}
+if (chetvert == 4)
+{
+    coords = "x > 0, y < 0";
+}
+return coords;
+}
+
+Console.WriteLine("Введи номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string coords = GetCoordArea(n);  // метод
+Console.WriteLine(coords);
+
---
+++ Seminar_3/Zadacha4/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4 */ 
+
+void PrintSquaresToConsole(int n)
+{
+    for (int i =1; i <=n; i++)
+    {
+        Console.WriteLine(i * i);
+    }
+}
+
+PrintSquaresToConsole(10);
