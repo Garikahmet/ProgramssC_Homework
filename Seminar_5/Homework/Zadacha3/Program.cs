f1250fec8ea016e7e655aa8076150f68d2906687
Igor Gainiiakhmetov <garikahmet@yandex.ru>
2022-11-16 21:28:49 +0200

    HM5

--- Seminar_5/Homework/Zadacha3/Program.cs
+++ Seminar_5/Homework/Zadacha3/Program.cs
@@ -1,50 +1,51 @@
 ﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76 */
 
-void FillArrayRandomNumbers(int[] numbers)
+void FillArrayRandomNumbers(double[] numbers)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    for(double i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = new Random().Next(100,1000);
+        numbers[(int)i] = new Random().NextDouble();
     }
+ return;  
 }
-void PrintArray(int[] numbers)
+void PrintArray(double[] numbers)
 {
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+    for(double i = 0; i < numbers.Length; i++)
     {
-        Console.Write(numbers[i] + " ");
+        Console.Write(numbers[(int)i] + " ");
     }
     Console.Write("]");
     Console.WriteLine();
 }
 
 Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-double[] numbers = new double[size];
+double size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[(int)size];
 FillArrayRandomNumbers(numbers);
 Console.WriteLine("Случайный массив: ");
 PrintArray(numbers);
 
-int minNumber = numbers[0];
-int maxNumber = numbers[0];
+double minNumber = numbers[0];
+double maxNumber = numbers[0];
 
-for (int i = 1; i < numbers; i++)
+for (double i = 1; i > numbers.Length; i++)
 {
-    if (numbers[i] < minNumber)
+    if (numbers[(int)i] < minNumber)
     {
-        minNumber = numbers[i];
-    }
+        minNumber = numbers[(int)i];
+    }  
 }
 
-for (int i = 1; i < numbers; i++)
+for (double i = 1; i < numbers.Length; i++)
 {
-    if (numbers[i] > maxNumber)
+    if (numbers[(int)i] > maxNumber)
     {
-        maxNumber = numbers[i];
+        maxNumber = numbers[(int)i];
     }
 }
-int result;
+double result;
 result = maxNumber - minNumber;
 
 Console.WriteLine($"Разница между max и min элементов массива: {result}");
