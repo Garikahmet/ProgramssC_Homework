f565d4289ecd1fb4b42f04082baf50b545ff52cd
Igor Gainiiakhmetov <garikahmet@yandex.ru>
2022-11-21 10:38:49 +0200

    Seminar_6

---
+++ Seminar_6/Zadacha3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10 */ 
+
+Console.Write("Введите число: ");
+int dec = Convert.ToInt32(Console.ReadLine());
+string ConvertToBin(int a)
+{
+    string bin1 = Convert.ToString(a);
+    string bin2 = "";
+    while (a > 0)
+    {
+        bin1 = (a % 2) + bin2;
+        a = a / 2;
+    }
+    return bin2;
+}
+Console.WriteLine(ConvertToBin(dec));
