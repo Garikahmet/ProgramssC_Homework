c90a4a6e1acaabf7e6a62f1ac93417cc4d9b46d3
Igor Gainiiakhmetov <garikahmet@yandex.ru>
2022-11-23 22:57:32 +0100

    Seminar_8

---
+++ Seminar_8/Zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждой строке.
+
+int[,] array = new int[3, 3];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    double tempValue = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 6);
+        Console.Write(array[i, j] + " ");
+
+        tempValue = tempValue + array[i, j];
+    }
+    Console.Write($"Ср. ар. строки = {tempValue / array.GetLength(1)}");
+    Console.WriteLine();
+}
---
+++ Seminar_8/Zadacha3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] ChangeArray(int[,] array)
+{
+    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+    for ( int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result[j, i] = array[i, j];
+        }
+    }
+    return result;
+}
+
+int[,] array = new int[3, 3];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 6);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+int[,] resultArray = ChangeArray(array);  
+for (int i = 0; i < resultArray.GetLength(0); i++)
+{
+    for (int j = 0; j < resultArray.GetLength(1); j++)
+    {
+        Console.Write(resultArray[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
